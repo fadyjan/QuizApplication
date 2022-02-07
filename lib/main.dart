d93c89cff3e09cd0b31ab606b565bbeb8840cd3a
fady jan <fady.ashraf@spimesenselabs.com>
2022-02-07 21:10:06 +0200

    Refresh The Quizz Logic

--- lib/main.dart
+++ lib/main.dart
@@ -17,31 +17,65 @@ class _MyAppState extends State<MyApp> {
   _xyz() {
     setState(() {
       _InitialIndex = _InitialIndex + 1;
-      if (_InitialIndex > 2) {
-        _InitialIndex = 0;
-      }
     });
   }
 
+  _ReloadQuiz() {
+    setState(() {
+      _InitialIndex = 0;
+    });
+  }
+
+  final QuestionsArray = const [
+    {
+      'questionsText': "what\'s your favorite color ?",
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionsText': "what\'s your favorite animal ?",
+      'answers': ['Lion', 'Tiger', 'Dog', 'Panther'],
+    },
+    {
+      'questionsText': "what\'s your favorite Film ?",
+      'answers': ['Interstiller', 'batman', 'avengars'],
+    },
+  ];
   Widget build(BuildContext context) {
-    const QuestionsArray = ["FirstQuestion", "SecondQuestion", "ThirdQuestion"];
+    // const QuestionsArray = ["FirstQuestion", "SecondQuestion", "ThirdQuestion"];
+    // ignore: non_constant_identifier_names
 
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
-          title: Text("First App"),
+          title: Text(
+            "Quiz App",
+          ),
+          centerTitle: true,
           backgroundColor: Colors.blue,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Question(QuestionsArray[_InitialIndex]),
-            Answers(_xyz),
-            Answers(_xyz),
-            Answers(_xyz),
-          ],
-        ),
+        body: _InitialIndex < QuestionsArray.length
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Question(
+                      QuestionsArray[_InitialIndex]['questionsText'] as String),
+                  ...(QuestionsArray[_InitialIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answers(_xyz, answer);
+                  }).toList(),
+                ],
+              )
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text("You Did IT !! :D"),
+                  ElevatedButton(
+                      onPressed: _ReloadQuiz, child: const Text("Reload Quiz"))
+                ],
+              ),
       ),
     );
   }
